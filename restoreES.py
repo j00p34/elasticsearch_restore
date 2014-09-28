8e4d9e44a0ea8a26c9626d974519cf35db60d4b1
zadelhoff <zadelhoff@fdmediagroep.nl>
2014-09-28 06:02:33 +0200

    cleaned up

--- restoreES.py
+++ restoreES.py
@@ -5,8 +5,6 @@ __author__ = 'zadelhoff'
 import urllib2
 import json
 
-data = ""
-
 method = "POST"
 
 handler = urllib2.HTTPHandler()
@@ -15,24 +13,23 @@ opener = urllib2.build_opener(handler)
 
 
 def openUrl(opener, request):
-    global data
     try:
         connection = opener.open(request)
-    except urllib2.HTTPError,e:
+    except urllib2.HTTPError, e:
         connection = e
 
     # check. Substitute with appropriate HTTP code.
     if connection.code == 200:
         data = connection.read()
-        return True
+        return True, data
     else:
         data = connection.read()
-        print data
-        return False
+        return False, data
         # handle the error case. connection.read() will still contain data
-         # if any was returned, but it probably won't be of any use
+        # if any was returned, but it probably won't be of any use
 
-def close( index ):
+
+def close(index):
     url = 'http://localhost:9200/' + index + '/_close'
     request = urllib2.Request(url, data=None)
     # overload the get method function with a small anonymous function...
@@ -41,20 +38,27 @@ def close( index ):
 
 
 request0 = urllib2.Request('http://localhost:9200/_stats/indexes?pretty', data=None)
-openUrl(opener, request0)
 
-for k in json.loads(data)['indices']:
-    close(k)
+indexes = openUrl(opener, request0)
+if (indexes[0]):
+    for k in json.loads(indexes[1])['indices']:
+        close(k)
 
-request3 = urllib2.Request('http://localhost:9200/_snapshot/prod_s3_repository/backup/_restore', data=None)
-request3.get_method = lambda: method
+    restoreRequest = urllib2.Request('http://localhost:9200/_snapshot/prod_s3_repository/backup/_restore', data=None)
+    restoreRequest.get_method = lambda: method
 
-if (openUrl(opener, request3)):
-    with open('/var/tmp/ESrestoreStatus', 'w') as f:
-        f.write(data)
+    restoreResult = openUrl(opener, restoreRequest)
+
+    if (restoreResult[0]):
+        with open('/var/tmp/ESrestoreStatus', 'w') as f:
+            f.write(restoreResult[1])
+    else:
+#        with open('/var/log/elasticsearch/restore.log', 'a') as f:
+        with open('/tmp/restore.log', 'a') as f:
+            f.write(restoreResult[1] + "\n")
 else:
     with open('/var/log/elasticsearch/restore.log', 'a') as f:
-        f.write(data + "\n")
+        f.write(indexes[1] + "\n")
 
 
 
